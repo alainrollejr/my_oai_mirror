c9fa2c7c7cf6134203a87d4490a4dcec765da7c9
Romain Beurdouche <romain.beurdouche@eurecom.fr>
2024-04-11 10:01:33 +0200

    feat(ldpc-decoding-module-interface-rework): slot decoding with T2

--- openair1/SIMULATION/NR_PHY/dlsim.c
+++ openair1/SIMULATION/NR_PHY/dlsim.c
@@ -38,6 +38,7 @@
 #include "PHY/defs_nr_UE.h"
 #include "PHY/phy_vars_nr_ue.h"
 #include "PHY/types.h"
+#include "PHY/CODING/nr_ulsch_decoding_interface.h"
 #include "PHY/INIT/nr_phy_init.h"
 #include "PHY/MODULATION/modulation_eNB.h"
 #include "PHY/MODULATION/nr_modulation.h"
